3633620acced298c9c855a29faaae2363bbea50e
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-12 18:12:11 +0530

    created the RoleRepository

---
+++ src/main/java/com/company/CareBridge/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.company.CareBridge.repository;
+
+import com.company.CareBridge.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+    Optional<Role> findByName(String name);
+}
