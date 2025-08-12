f13aa59ea978a0016eb6089865faef21fe203728
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-12 18:17:03 +0530

    added the RoleSeeder for the RBAC

---
+++ src/main/java/com/company/CareBridge/config/RoleSeeder.java
@@ -0,0 +1,39 @@
+package com.company.CareBridge.config;
+
+
+import com.company.CareBridge.entity.Role;
+import com.company.CareBridge.repository.PermissionRepository;
+import com.company.CareBridge.repository.RoleRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class RoleSeeder {
+
+    private final RoleRepository roleRepository;
+    private final PermissionRepository  permissionRepository;
+
+    @PostConstruct
+    public void seedRoles(){
+        if(roleRepository.findByName("ADMIN").isEmpty()){
+            Role admin = new Role();
+            admin.setName("ADMIN");
+            admin.setPermissions(Set.copyOf(permissionRepository.findAll()));
+            roleRepository.save(admin);
+        }
+
+        if (roleRepository.findByName("NGO").isEmpty()) {
+            Role ngo = new Role();
+            ngo.setName("NGO");
+            ngo.setPermissions(Set.of(
+                    permissionRepository.findByName("READ_CASE").orElseThrow(),
+                    permissionRepository.findByName("CREATE_REFERRAL").orElseThrow()
+            ));
+            roleRepository.save(ngo);
+        }
+    }
+}
