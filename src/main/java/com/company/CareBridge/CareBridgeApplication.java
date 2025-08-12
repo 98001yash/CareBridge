badef37ce3261559208eea5d935b8627c5eb4d0e
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-12 16:39:30 +0530

    first commit

---
+++ src/main/java/com/company/CareBridge/CareBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.company.CareBridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareBridgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareBridgeApplication.class, args);
+	}
+
+}
