2e3535c0a3e37721d1acc7d7d6c9c0deafaaa407
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-13 18:21:59 +0530

    created the controller for the donation

--- src/main/java/com/company/CareBridge/controller/DonationController.java
+++ src/main/java/com/company/CareBridge/controller/DonationController.java
@@ -3,12 +3,15 @@ package com.company.CareBridge.controller;
 
 import com.company.CareBridge.dtos.DonationRequestDto;
 import com.company.CareBridge.dtos.DonationResponseDto;
+import com.company.CareBridge.dtos.DonationStatusUpdateDto;
 import com.company.CareBridge.service.DonationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/donations")
@@ -27,5 +30,27 @@ public class DonationController {
         log.info("Received request to create donation for donorId {}",donorId);
         DonationResponseDto createdDonation = donationService.createDonation(donorId, donationRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdDonation);
+    }
+
+    @GetMapping("/donor/{donorId}")
+    public ResponseEntity<List<DonationResponseDto>> getDonorDonations(@PathVariable Long donorId) {
+        log.info("Received request to fetch donations for donorId {}", donorId);
+        return ResponseEntity.ok(donationService.getDonorDonations(donorId));
+    }
+
+    @GetMapping("/ngo/{ngoId}")
+    public ResponseEntity<List<DonationResponseDto>> getNgoDonations(@PathVariable Long ngoId) {
+        log.info("Received request to fetch donations for ngoId {}", ngoId);
+        return ResponseEntity.ok(donationService.getNgoDonations(ngoId));
+    }
+
+
+    @PatchMapping("/{donationId}/status")
+    public ResponseEntity<DonationResponseDto> updateDonationStatus(
+            @PathVariable Long donationId,
+            @RequestBody DonationStatusUpdateDto statusUpdateDto) {
+        log.info("Received request to update status for donationId {}", donationId);
+        DonationResponseDto updatedDonation = donationService.updateDonationStatus(donationId, statusUpdateDto);
+        return ResponseEntity.ok(updatedDonation);
     }
 }
