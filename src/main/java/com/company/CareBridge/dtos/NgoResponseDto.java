aeece4b8a9429feba4ace32b66dbf26fe26b463f
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-13 21:16:44 +0530

    added the NgoResponseDto

---
+++ src/main/java/com/company/CareBridge/dtos/NgoResponseDto.java
@@ -0,0 +1,27 @@
+package com.company.CareBridge.dtos;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class NgoResponseDto {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
