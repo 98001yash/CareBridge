8f6be3dc8fd92c919cff436bdef6164cdc710201
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-14 18:04:54 +0530

    created the donationRequestResponseDto

--- src/main/java/com/company/CareBridge/dtos/DonationRequestRequestDto.java
+++ src/main/java/com/company/CareBridge/dtos/DonationRequestRequestDto.java
@@ -1,26 +1,19 @@
 package com.company.CareBridge.dtos;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
-
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class DonationRequestResponseDto {
+public class DonationRequestRequestDto {
 
-    private Long id;
     private String title;
     private String description;
     private String category;
     private Integer quantity;
-    private Long ngoId;
-    private String ngoName;
-    private LocalDateTime createdAt;
 }
