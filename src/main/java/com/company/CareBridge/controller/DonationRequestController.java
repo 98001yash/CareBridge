5619c00233aaedebcb9b81f97629ae8bddcbc8f6
Yash Chauhan <yashchauhan.gaya@gmail.com>
2025-08-14 19:02:20 +0530

    created the getAllDonationRequst

--- src/main/java/com/company/CareBridge/controller/DonationRequestController.java
+++ src/main/java/com/company/CareBridge/controller/DonationRequestController.java
@@ -8,10 +8,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/donation-requests")
@@ -26,5 +25,11 @@ public class DonationRequestController {
         log.info("Received request to create donation request");
         DonationRequestResponseDto created = donationRequestService.createDonationRequest(requestDto);
         return ResponseEntity.ok(created);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DonationRequestResponseDto>> getAllDonationRequests(){
+        log.info("Received request to fetch all donation requests");
+        return ResponseEntity.ok(donationRequestService.getAllDonationRequests());
     }
 }
